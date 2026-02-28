ad7001f0ebd2f45d6126e6597f896e27e4c515a2
shibahex <badamsva@gmail.com>
2026-02-27 21:46:46 -0500

    initial commit

---
+++ flake.nix
@@ -0,0 +1,74 @@
+{
+  description = "Hack The Box flake [.#full .#burp]";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    # Optional burp shell will use this
+    burpsuitepro = {
+      url = "github:xiv3r/Burpsuite-Professional";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      burpsuitepro,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+
+      # Core CLI tooling (lightweight)
+      basePackages = with pkgs; [
+        openvpn
+        nmap
+        nushell
+        sqlite
+        hashcat
+        unzip
+        awscli2
+        ffuf
+      ];
+
+      # Burp package (only used in specific shells)
+      burpPackage = burpsuitepro.packages.${system}.default;
+    in
+    {
+      devShells.${system} = {
+
+        # Default HTB shell (recommended everyday shell)
+        default = pkgs.mkShell {
+          packages = basePackages;
+        };
+
+        # Web testing shell (adds Burp)
+        burp = pkgs.mkShell {
+          packages = basePackages ++ [ burpPackage ];
+
+          buildInputs = with pkgs; [
+            fontconfig
+          ];
+
+          shellHook = ''
+            export FONTCONFIG_FILE=${pkgs.fontconfig.out}/etc/fonts/fonts.conf
+          '';
+        };
+
+        # Everything included
+        full = pkgs.mkShell {
+          packages = basePackages ++ [ burpPackage ];
+
+          buildInputs = with pkgs; [
+            fontconfig
+          ];
+
+          shellHook = ''
+            export FONTCONFIG_FILE=${pkgs.fontconfig.out}/etc/fonts/fonts.conf
+          '';
+        };
+      };
+    };
+}
