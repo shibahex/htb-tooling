7c379da974ca68311f2a7cb1f823316cd97f3e85
shibahex <badamsva@gmail.com>
2026-05-08 15:46:16 -0400

    feat(shells): add modular helper func mkCombined and add reverse engineer packages

--- flake.nix
+++ flake.nix
@@ -1,5 +1,6 @@
 {
-  description = "Hack The Box flake [.#full .#burp or .#master-default .#master-full]";
+  description = "Hack The Box flake [.#full .#burp .#reverse .#automation or .#master-*]";
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-master.url = "github:NixOS/nixpkgs/master";
@@ -9,48 +10,127 @@
     };
   };
 
-  outputs = { self, nixpkgs, nixpkgs-master, burpsuitepro }:
+  outputs =
+    { self
+    , nixpkgs
+    , nixpkgs-master
+    , burpsuitepro
+    ,
+    }:
     let
       system = "x86_64-linux";
       burpPackage = burpsuitepro.packages.${system}.default;
 
-      mkPkgs = input: import input {
-        inherit system;
-        config.allowUnfree = true;
-      };
-
-      mkShells = pkgs: {
-        default = pkgs.mkShell {
-          packages = basePackages pkgs;
+      mkPkgs =
+        input:
+        import input {
+          inherit system;
+          config.allowUnfree = true;
         };
-        burp = pkgs.mkShell {
-          packages = basePackages pkgs ++ [ burpPackage ];
+
+      mkShellHook = title: pkgList: extra: ''
+        printf "\n"
+        printf "\033[1;36m[ \033[1;33m${title}\033[1;36m ]\033[0m\n"
+        printf "\033[2m─────────────────────────────\033[0m\n"
+        ${builtins.concatStringsSep "\n" (
+          map (p: "printf \"  \\033[1;32m+\\033[0m ${p.name}\\n\"") pkgList
+        )}
+        printf "\033[2m─────────────────────────────\033[0m\n\n"
+        ${extra}
+        exec nu
+      '';
+
+      mkCombined =
+        pkgs: name: pkgLists: extra:
+        let
+          allPkgs = builtins.concatLists pkgLists;
+        in
+        pkgs.mkShell {
+          packages = allPkgs;
           buildInputs = [ pkgs.fontconfig ];
-          shellHook = ''
+          shellHook = mkShellHook name allPkgs extra;
+        };
+
+      mkShells =
+        pkgs:
+        let
+          fontHook = ''
             export FONTCONFIG_FILE=${pkgs.fontconfig.out}/etc/fonts/fonts.conf
           '';
+          shellList = builtins.concatStringsSep "\n" (
+            map (name: "printf \"  \\033[1;32m->\\033[0m \\033[1m${name}\\033[0m\\n\"") (
+              builtins.attrNames (mkShells pkgs)
+            )
+          );
+        in
+        {
+          # to add a shell:
+          # myShell = mkCombined pkgs "myShell" [ (basePackages pkgs) (myPackages pkgs) ] "";
+          # to combine with existing:
+          # myShell = mkCombined pkgs "myShell" [ (basePackages pkgs) (myPackages pkgs) [ burpPackage ] ] fontHook;
+
+          default = mkCombined pkgs "default" [ (basePackages pkgs) ] ''
+            printf "\033[1;33m  shells:\033[0m\n"
+            ${shellList}
+            printf "\n\033[2m  prefix master- for master channel (gets a lot of new patches)\033[0m\n"
+          '';
+
+          burp = mkCombined pkgs "burp" [
+            (basePackages pkgs)
+            [ burpPackage ]
+          ]
+            fontHook;
+
+          reverse = mkCombined pkgs "reverse-engineering" [
+            (basePackages pkgs)
+            (reversePackages pkgs)
+          ] "printf '[+] Loaded reversing environment\n'";
+
+          automation = mkCombined pkgs "automation" [
+            (basePackages pkgs)
+            (automationPackages pkgs)
+          ] "printf '[+] Loaded msf, wpscan and netexec\n'";
+
+          full = mkCombined pkgs "full" [
+            (basePackages pkgs)
+            [ burpPackage ]
+            (reversePackages pkgs)
+            (automationPackages pkgs)
+          ]
+            fontHook;
         };
-      } // { full = (mkShells pkgs).burp; };
 
-      basePackages = pkgs: with pkgs; [
-        openvpn nmap nushell sqlite hashcat
-        unzip awscli2 ffuf inetutils exploitdb
-        gobuster
-      ] ++ frameworks pkgs;
+      basePackages =
+        pkgs: with pkgs; [
+          nushell
+          openvpn
+          nmap
+          hashcat
+          ffuf
+          inetutils
+          exploitdb
+          gobuster
+        ];
 
-      frameworks = pkgs: with pkgs; [
-        metasploit wpscan netexec
-      ];
+      automationPackages =
+        pkgs: with pkgs; [
+          metasploit
+          wpscan
+          netexec
+        ];
 
-      prefixAttrs = prefix: attrs:
-        nixpkgs.lib.mapAttrs' (k: v:
-          nixpkgs.lib.nameValuePair "${prefix}-${k}" v
-        ) attrs;
+      reversePackages =
+        pkgs: with pkgs; [
+          cutter
+          pince
+        ];
+
+      prefixAttrs =
+        prefix: attrs: nixpkgs.lib.mapAttrs' (k: v: nixpkgs.lib.nameValuePair "${prefix}-${k}" v) attrs;
 
     in
     {
       devShells.${system} =
-        mkShells (mkPkgs nixpkgs) //
-        prefixAttrs "master" (mkShells (mkPkgs nixpkgs-master));
+        mkShells (mkPkgs nixpkgs) // prefixAttrs "master" (mkShells (mkPkgs nixpkgs-master));
     };
 }
